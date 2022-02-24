b80ecf477063d8c9ead72b6ede483905c1e265ff
Jamie Bunch <JamieBunch@ymail.com>
2022-02-24 10:55:42 -0500

    add index.ts for interface

---
+++ index.ts
@@ -0,0 +1,26 @@
+interface User {
+    id: string;
+    username: string;
+}
+
+interface Message {
+    id: string;
+    userId: string;
+    text: string;
+    sentAt: Date;
+}
+
+
+const createUser = (user: User) => {
+
+}
+
+const sendMessage = (message: Message) => {
+
+}
+
+const getMessages = (): Message[] => {
+
+    return [];
+}
+
